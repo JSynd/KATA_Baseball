9f39902157073a6f3e4412f4b2bcb749139255d6
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-30 16:02:32 +0900

    [refactor] add funtions for composing methods

--- KATA_Baseball/baseball.cpp
+++ KATA_Baseball/baseball.cpp
@@ -13,20 +13,22 @@ class Baseball {
 public:
 	const int LEN_ANSWER = 3;
 
-	explicit Baseball(const string& answer) : answer(answer) {}
+	explicit Baseball(const string& answer) : answer(answer), strike_cnt(0), ball_cnt(0) {}
 	Result guess(const string& guessNumber) {
 		setGuessValue(guessNumber);
-		
-		checkValidity();
 
-		int strikeCnt = getStrikeCount();
-		if (strikeCnt == 0) return { false, 0, 2 };
-		return { strikeCnt == LEN_ANSWER, strikeCnt , 0 };
+		checkValidity();
+		
+		checkAnswer();
+		return makeResult();
 	}
 
 private:
 	string answer;
 	string guessValue;
+
+	int strike_cnt;
+	int ball_cnt;
 
 	void setGuessValue(const string& guessNumber) {
 		guessValue = guessNumber;
@@ -53,11 +55,30 @@ private:
 			|| guessValue[1] == guessValue[2];
 	}
 
+	void checkAnswer() {
+		strike_cnt = getStrikeCount();
+		ball_cnt = getBallCount();
+	}
+
 	int getStrikeCount() {
 		int num_strikes = 0;
 		for (int i = 0; i < answer.size(); i++) {
 			if (answer[i] == guessValue[i]) num_strikes++;
 		}
 		return num_strikes;
+	}
+
+	int getBallCount() {
+		int num_balls = 0;
+		for (int i = 0; i < answer.size(); i++) {
+			for (int j = 0; j < answer.size(); j++) {
+				if (i != j && answer[i] == guessValue[j]) num_balls++;
+			}
+		}
+		return num_balls;
+	}
+
+	Result makeResult() {
+		return { strike_cnt == LEN_ANSWER, strike_cnt , ball_cnt };
 	}
 };
