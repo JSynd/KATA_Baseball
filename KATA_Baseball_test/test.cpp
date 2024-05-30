e5a3f09178d6614785fabb68cd54b77feffde489
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-30 14:34:26 +0900

    [chore] fix typo

--- KATA_Baseball_test/test.cpp
+++ KATA_Baseball_test/test.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include "../KATA_Baseball/Baseball.cpp"
 
+TEST(BaseballGame, ThrowExeptionWhenInputLengthNotMatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
+
 TEST(BaseballGame, ThrowExeptionWhenInputLenthNotMatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
