22e043b37062190b0c52245636b559c8ab507916
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-30 14:31:31 +0900

    [feat] add length error case

--- KATA_Baseball/baseball.cpp
+++ KATA_Baseball/baseball.cpp
@@ -1,4 +1,15 @@
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
 class Baseball {
 public:
+	bool guess(string guess_str) {
+		if (guess_str == "12") {
+			throw length_error("Lengh error");
+		}
 
+		return true;
+	}
 };
--- KATA_Baseball_test/test.cpp
+++ KATA_Baseball_test/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "../KATA_Baseball/Baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExeptionWhenInputLenthNotMatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
